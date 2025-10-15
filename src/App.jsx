f38f91f1e41b4a256d63ce6cad32ecd4020fd09d
Jeremiah <jeremiahelachi234@gmail.com>
2025-10-15 09:49:27 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from "react";
+
+const App = () => {
+  const [advice, setAdvice] = useState(null);
+  const fetchAdvice = async () => {
+    try {
+      const response = await fetch("https://api.adviceslip.com/advice");
+      const data = await response.json();
+      console.log(data);
+      setAdvice(data.slip);
+    } catch (error) {
+      console.error("Error fetching advice:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchAdvice();
+  }, []);
+  return (
+    <>
+      <div className="container">
+        <h1 className="heading">Advice #{advice?.id ?? "..."}</h1>
+        <p className="advice">"{advice?.advice ?? "Loading..."}"</p>
+        <div className="divider">
+          <img src="/images/pattern-divider-desktop.svg" alt="divider" />
+        </div>
+        <button className="dice-button" onClick={fetchAdvice}>
+          <img src="/images/icon-dice.svg" alt="dice" />
+        </button>
+
+      </div>
+      
+    </>
+  );
+};
+
+export default App;
